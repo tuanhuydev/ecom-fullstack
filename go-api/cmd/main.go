f862f16bdd90686f2a190d4e101f5d4755ff8646
huy_nguyen <huy_nguyen@epam.com>
2025-03-11 15:08:58 +0700

    init project

---
+++ go-api/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "OK",
+		})
+	})
+	server.Run()
+}
